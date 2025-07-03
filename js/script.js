4132f16ed8436c0103d28025709f2adce5be251f
cassiostersi <cassioneto@ifsp.edu.br>
2025-07-03 10:53:53 -0300

    alteracao

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector(".accordion-toggle");
+const content = document.querySelector(".accordion-content");
+
+toggle.addEventListener("click", () => {
+  content.classList.toggle("open");
+});
